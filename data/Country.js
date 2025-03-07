c6d7b8b8cc455cd2820479f867d847761c3d9cbf
librossard <lilian.brossard@etudiant.univ-rennes1.fr>
2025-03-08 00:15:51 +0100

    version test

--- data/Country.js
+++ data/Country.js
@@ -1,74 +1,97 @@
 class Country {
-    constructor(alpha3Code, name, capital, region, population, borders) {
-        this.alpha3Code = alpha3Code;
-        this.name = name;
-        this.capital = capital;
-        this.region = region;
-        this.population = population;
-        this.borders = borders;
-    }
+  constructor(alpha3Code, name, capital, region, population, borders) {
+    this.alpha3Code = alpha3Code;
+    this.name = name;
+    this.capital = capital;
+    this.region = region;
+    this.population = population;
+    this.borders = borders;
+  }
 
-    toString() {
-        return `${this.alpha3Code}, ${this.name}, ${this.capital}, ${this.region}, ${this.population}, ${this.borders.join(', ')})`;
-    }
+  toString() {
+    return `${this.alpha3Code}, ${this.name}, ${this.capital}, ${
+      this.region
+    }, ${this.population}, ${this.borders.join(", ")})`;
+  }
 
-    static fill_countries() {
-        this.all_countries = {};
-        countries.forEach(country => {
-            const alpha3Code = country.alpha3Code ? country.alpha3Code : "N/A";
-            const name = country.translations ? country.translations.fr : "N/A";
-            const capital = country.capital ? country.capital : "N/A";
-            const region = country.region ? country.region : "N/A";
-            const population = country.population ? country.population : "N/A";
-            const borders = country.borders ? country.borders : [];
-            if (alpha3Code != "N/A" && this.all_countries[alpha3Code] == undefined) {
-                const countryObj = new Country(alpha3Code, name, capital, region, population, borders);
-                this.all_countries[alpha3Code] = countryObj;
-            } else if(alpha3Code == "N/A") {
-                console.error("Erreur avec le pays de code " + alpha3Code + " et de nom " + name + " : code alpha3 manquant");
-            }
-        });
+  static fill_countries() {
+    this.all_countries = {};
+    countries.forEach((country) => {
+      const alpha3Code = country.alpha3Code ? country.alpha3Code : "N/A";
+      const name = country.translations ? country.translations.fr : "N/A";
+      const capital = country.capital ? country.capital : "N/A";
+      const region = country.region ? country.region : "N/A";
+      const population = country.population ? country.population : "N/A";
+      const borders = country.borders ? country.borders : [];
+      if (alpha3Code != "N/A" && this.all_countries[alpha3Code] == undefined) {
+        const countryObj = new Country(
+          alpha3Code,
+          name,
+          capital,
+          region,
+          population,
+          borders
+        );
+        this.all_countries[alpha3Code] = countryObj;
+      } else if (alpha3Code == "N/A") {
+        console.error(
+          "Erreur avec le pays de code " +
+            alpha3Code +
+            " et de nom " +
+            name +
+            " : code alpha3 manquant"
+        );
+      }
+    });
+  }
+  getPopDensity() {
+    const country = countries.find((c) => c.alpha3Code === this.alpha3Code);
+    const area = country.area;
+    let result = Number(this.population) / Number(area);
+    if (isNaN(result)) {
+      return "N/A";
     }
-    getPopDensity() {
-        if (this.population && this.area && this.population != "N/A" && this.area != "N/A") {
-            return this.population / this.area;
+    return result;
+  }
+
+  getBorders() {
+    return this.borders;
+  }
+
+  getCurrencies() {
+    let currencies = [];
+    for (let i = 0; i < countries.length; i++) {
+      if (
+        countries[i].alpha3Code == this.alpha3Code &&
+        countries[i].currencies != undefined
+      ) {
+        for (let j = 0; j < countries[i].currencies.length; j++) {
+          const currency =
+            Currency.all_currencies[countries[i].currencies[j].code];
+          if (currency) {
+            currencies.push(currency);
+          }
         }
-        return "N/A";
+        break;
+      }
     }
+    return currencies;
+  }
 
-    getBorders() {
-        return this.borders;
-    }
-
-    getCurrencies() {
-        let currencies = []
-        for (let i = 0; i < countries.length; i++) {
-            if (countries[i].alpha3Code == this.alpha3Code) {
-                for (let j = 0; j < countries[i].currencies.length; j++) {
-                    const currency = Currency.all_currencies[countries[i].currencies[j].code];
-                    if (currency) {
-                        currencies.push(currency);
-                    }
-                }
-                break;
-            }
+  getLanguages() {
+    let language = [];
+    for (let i = 0; i < countries.length; i++) {
+      if (countries[i].alpha3Code == this.alpha3Code) {
+        for (let j = 0; j < countries[i].languages.length; j++) {
+          const currency =
+            Language.all_languages[countries[i].languages[j].iso639_2];
+          if (currency) {
+            language.push(currency);
+          }
         }
-        return currencies ? currencies : [];
+        break;
+      }
     }
-
-    getLanguages() {
-        let language = [];
-        for (let i = 0; i < countries.length; i++) {
-            if (countries[i].alpha3Code == this.alpha3Code) {
-                for (let j = 0; j < countries[i].languages.length; j++) {
-                    const currency = Language.all_language[countries[i].language[j].code];
-                    if (currency) {
-                        language.push(currency);
-                    }
-                }
-                break;
-            }
-        }
-        return language ? language : [];
-    }
+    return language;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,23 +1,164 @@
-// Remplir le tableau associatif
-Currency.fill_currencies();
+Currency.fill_currencies(); // Remplir le tableau associatif
+// console.table(Currency.all_currencies); // Afficher les devises
 
-// Afficher les devises
-console.table(Currency.all_currencies);
+Language.fill_languages(); // Remplir le tableau associatif des langues
+// console.table(Language.all_languages); // Afficher les langues
 
-// Remplir le tableau associatif des langues
-Language.fill_languages();
+Country.fill_countries(); // Remplir le tableau associatif des pays
+// console.table(Country.all_countries); // Afficher les pays
 
-// Afficher les langues
-console.table(Language.all_languages);
+function clearlogs() {
+  console.clear();
+}
+function outsideTheContinent() {
+  let result = [];
+  Object.values(Country.all_countries).forEach((pays) => {
+    pays.getBorders().forEach((border) => {
+      if (
+        Country.all_countries[border].region != pays.region &&
+        !result.includes(pays)
+      ) {
+        result.push(pays);
+      }
+    });
+  });
+  console.table(result);
+}
 
-// Remplir le tableau associatif des pays
-Country.fill_countries();
+function moreNeighbors() {
+  let maxVoisins = 0;
+  let countriesWithMostVoisins = [];
 
-// Afficher les pays
-console.table(Country.all_countries);
+  Object.values(Country.all_countries).forEach((country) => {
+    if (country.getBorders().length > maxVoisins) {
+      maxVoisins = country.getBorders().length;
+      countriesWithMostVoisins = [country];
+    } else if (country.getBorders().length === maxVoisins) {
+      countriesWithMostVoisins.push(country);
+    }
+  });
 
-// Utilisation de la fonction getCurrencies
-console.log(Country.all_countries["AFG"].getCurrencies());
+  console.table(
+    countriesWithMostVoisins.map((country) => {
+      return {
+        pays: country.name,
+        vosins: country
+          .getBorders()
+          .map((border) => Country.all_countries[border].name),
+      };
+    })
+  );
+}
 
-// Utilisation de la fonction getLanguages
-console.log(Country.all_countries["AFG"].getLanguages());
+function neighborless() {
+  let result = Object.values(Country.all_countries).filter(
+    (country) => country.getBorders().length === 0
+  );
+  console.table(result.map((country) => country.name));
+}
+
+function moreLanguages() {
+  let maxLanguages = 0;
+  let countriesWithMostLanguages = [];
+  Object.values(Country.all_countries).forEach((pays) => {
+    if (pays.getLanguages().length > maxLanguages) {
+      maxLanguages = pays.getLanguages().length;
+      countriesWithMostLanguages = [pays];
+    } else if (pays.getLanguages().length === maxLanguages) {
+      countriesWithMostLanguages.push(pays);
+    }
+  });
+
+  console.table(
+    countriesWithMostLanguages.map((countrie) => {
+      return {
+        pays: countrie.name,
+        langues: countrie.getLanguages(),
+      };
+    })
+  );
+}
+
+function withCommonLanguage() {
+  let result = [];
+  Object.values(Country.all_countries).forEach((country) => {
+    commonLanguagesCountry = [];
+    commonLanguagesLang = [];
+    country.getLanguages().forEach((Language) => {
+      country.getBorders().forEach((paysVoisin) => {
+        if (
+          Country.all_countries[paysVoisin].getLanguages().includes(Language)
+        ) {
+          if (
+            !commonLanguagesCountry.includes(Country.all_countries[paysVoisin])
+          ) {
+            commonLanguagesCountry.push(Country.all_countries[paysVoisin]);
+          }
+          if (!commonLanguagesLang.includes(Language)) {
+            commonLanguagesLang.push(Language);
+          }
+        }
+      });
+    });
+    result.push({
+      pays: country.name,
+      paysVoisins: commonLanguagesCountry,
+      languesCommunes: commonLanguagesLang,
+    });
+  });
+  console.table(result);
+}
+
+function withoutCommonCurrency() {
+  let result = [];
+  Object.values(Country.all_countries).forEach((country) => {
+    country.getCurrencies().forEach((currency) => {
+      let commonCurrency = false;
+      country.getBorders().forEach((paysVoisin) => {
+        if (
+          Country.all_countries[paysVoisin].getCurrencies().includes(currency)
+        ) {
+          commonCurrency = true;
+        }
+      });
+      if (!commonCurrency && !result.includes(country.name)) {
+        result.push({
+          pays: country.name,
+        });
+      }
+    });
+  });
+  console.table(result);
+}
+
+function sortingDecreasingDensity() {
+  let countriesWithDensity = Object.values(Country.all_countries).filter(
+    (country) => country.getPopDensity() != "N/A"
+  );
+  let sortedCountries = Object.values(countriesWithDensity).sort((a, b) => {
+    return b.getPopDensity() - a.getPopDensity();
+  });
+
+  console.table(
+    sortedCountries.map((country) => {
+      return {
+        pays: country.name,
+        densite: Math.round(country.getPopDensity()),
+      };
+    })
+  );
+}
+
+function moreTopLevelDomains() {
+  result = [];
+  countries.forEach((country) => {
+    if (country.topLevelDomain.length > 1) {
+      result.push({
+        pays: country.translations.fr,
+        domaines: country.topLevelDomain,
+      });
+    }
+  });
+
+  console.table(result);
+}
