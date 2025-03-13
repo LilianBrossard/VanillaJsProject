48157682dac89e0aad4a7299f9d552a6e070bf9a
librossard <librossard@etudiant.univ-rennes1.fr>
2025-03-13 08:48:37 +0100

    chore : comment js

--- test/test.js
+++ test/test.js
@@ -10,14 +10,16 @@ Country.fill_countries(); // Remplir le tableau associatif des pays
 function clearlogs() {
   console.clear();
 }
+
+//pay qui possède pays frontalier qui n’est pas dans le même continent
 function outsideTheContinent() {
   let result = [];
-  Object.values(Country.all_countries).forEach((pays) => {
-    pays.getBorders().forEach((border) => {
+  Object.values(Country.all_countries).forEach((pays) => {//pour tout les objet de la classe Country
+    pays.getBorders().forEach((border) => {//pour chaque pays voisin
       if (
         border.region != pays.region &&
         !result.includes(pays)
-      ) {
+      ) {//si le pays voisin n'est pas dans le même continent que le pays et que le pays n'est pas déjà dans le tableau de resultat
         result.push(pays);
       }
     });
@@ -25,15 +27,16 @@ function outsideTheContinent() {
   console.table(result);
 }
 
+//pays qui possède le plus de voisins
 function moreNeighbors() {
   let maxVoisins = 0;
   let countriesWithMostVoisins = [];
 
-  Object.values(Country.all_countries).forEach((country) => {
-    if (country.getBorders().length > maxVoisins) {
+  Object.values(Country.all_countries).forEach((country) => {//pour tout les objet de la classe Country
+    if (country.getBorders().length > maxVoisins) {//si le pays a plus de voisins que le max alors c'est le nouveau max
       maxVoisins = country.getBorders().length;
       countriesWithMostVoisins = [country];
-    } else if (country.getBorders().length === maxVoisins) {
+    } else if (country.getBorders().length === maxVoisins) {//si le pays a autant de voisins que le max, ajouter a la liste deja existante
       countriesWithMostVoisins.push(country);
     }
   });
@@ -50,21 +53,23 @@ function moreNeighbors() {
   );
 }
 
+//pays qui ne possède pas de voisins
 function neighborless() {
-  let result = Object.values(Country.all_countries).filter(
-    (country) => country.getBorders().length === 0
+  let result = Object.values(Country.all_countries).filter(//pour tout les objet de la classe Country 
+    (country) => country.getBorders().length === 0 //ne garder que les pays qui n'ont pas de voisins
   );
   console.table(result.map((country) => country.name));
 }
 
+//pays qui possède le plus de langues
 function moreLanguages() {
   let maxLanguages = 0;
   let countriesWithMostLanguages = [];
-  Object.values(Country.all_countries).forEach((pays) => {
-    if (pays.getLanguages().length > maxLanguages) {
+  Object.values(Country.all_countries).forEach((pays) => {//pour tout les objet de la classe Country
+    if (pays.getLanguages().length > maxLanguages) {//si le pays a plus de langues que le max alors c'est le nouveau max
       maxLanguages = pays.getLanguages().length;
       countriesWithMostLanguages = [pays];
-    } else if (pays.getLanguages().length === maxLanguages) {
+    } else if (pays.getLanguages().length === maxLanguages) { //si le pays a autant de langues que le max, ajouter a la liste deja existante
       countriesWithMostLanguages.push(pays);
     }
   });
@@ -79,22 +84,23 @@ function moreLanguages() {
   );
 }
 
+//pays qui possède une langue en commun avec ses voisins
 function withCommonLanguage() {
   let result = [];
-  Object.values(Country.all_countries).forEach((country) => {
+  Object.values(Country.all_countries).forEach((country) => {//pour tout les objet de la classe Country
     commonLanguagesCountry = [];
     commonLanguagesLang = [];
-    country.getLanguages().forEach((Language) => {
-      country.getBorders().forEach((paysVoisin) => {
+    country.getLanguages().forEach((Language) => {//pour chaque langue du pays
+      country.getBorders().forEach((paysVoisin) => {//pour chaque pays voisin
         if (
           paysVoisin.getLanguages().includes(Language)
-        ) {
+        ) {//si le pays voisin parle la langue
           if (
             !commonLanguagesCountry.includes(paysVoisin)
-          ) {
+          ) {//si le pays voisin n'est pas deja dans le tableau de resultat
             commonLanguagesCountry.push(paysVoisin);
           }
-          if (!commonLanguagesLang.includes(Language)) {
+          if (!commonLanguagesLang.includes(Language)) {//si la langue n'est pas deja dans le tableau de resultat
             commonLanguagesLang.push(Language);
           }
         }
@@ -109,19 +115,20 @@ function withCommonLanguage() {
   console.table(result);
 }
 
+//pays qui ne possède pas de monnaie en commun
 function withoutCommonCurrency() {
   let result = [];
-  Object.values(Country.all_countries).forEach((country) => {
+  Object.values(Country.all_countries).forEach((country) => {//pour tout les objet de la classe Country
     country.getCurrencies().forEach((currency) => {
       let commonCurrency = false;
-      country.getBorders().forEach((paysVoisin) => {
+      country.getBorders().forEach((paysVoisin) => {//pour chaque pays voisin
         if (
           paysVoisin.getCurrencies().includes(currency)
-        ) {
+        ) {//si le pays voisin a la meme monnaie
           commonCurrency = true;
         }
       });
-      if (!commonCurrency && !result.some((item) => item.pays === country.name)) {
+      if (!commonCurrency && !result.some((item) => item.pays === country.name)) { //si le pays n'a pas de monnaie en commun
         result.push({
           pays: country.name,
         });
@@ -131,15 +138,15 @@ function withoutCommonCurrency() {
   console.table(result);
 }
 
+//pays trier par densité décroissante
 function sortingDecreasingDensity() {
-  let countriesWithDensity = Object.values(Country.all_countries).filter(
+  let countriesWithDensity = Object.values(Country.all_countries).filter(//pour tout les objet de la classe Country filtrer les pays qui ont une densité de population
     (country) => country.getPopDensity() != "N/A"
   );
-  let sortedCountries = Object.values(countriesWithDensity).sort((a, b) => {
+  let sortedCountries = Object.values(countriesWithDensity).sort((a, b) => {//les objet de la classe Country restants sont trier par densité de population
     return b.getPopDensity() - a.getPopDensity();
   });
-
-  console.table(
+  console.table( 
     sortedCountries.map((country) => {
       return {
         pays: country.name,
@@ -149,10 +156,11 @@ function sortingDecreasingDensity() {
   );
 }
 
+//pays avec le plusieurs domaines internet
 function moreTopLevelDomains() {
   result = [];
-  countries.forEach((country) => {
-    if (country.topLevelDomain.length > 1) {
+  countries.forEach((country) => {//pour tout les pays
+    if (country.topLevelDomain.length > 1) {//si le pays a plusieurs domaines internet
       result.push({
         pays: country.translations.fr,
         domaines: country.topLevelDomain,
